f9df7c25d12eca729d230c88a8c22fe926cf6e2c
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-24 00:01:30 -0600

    adding css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,13 +6,25 @@
 		<!--[if lt IE 9]><script
 		src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![end
 		if]-->
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<h1>Jennifer Sukay</h1>
-		<ul>
-			<li><p><span id="lead">Purpose</span>: Offer light business counseling services and website design.</p></li>
-			<li><p><span id="lead">Audience</span>: Entrepreneurs looking to have their website designed.</p></li>
-			<li><p><span id="lead">Goal</span>: Attract part-time employment through a business or potential freelance clients.</p></li>
-		</ul>
+		<header><!--branding and navigation area-->
+			<h1>Jennifer Sukay</h1>
+		</header>
+		<main>
+		<article>
+		<!--main review goes here-->
+		<section>
+			<ul>
+				<li><p><span id="lead">Purpose</span>: Offer light business counseling services and website design.</p></li>
+				<li><p><span id="lead">Audience</span>: Entrepreneurs looking to have their website designed.</p></li>
+				<li><p><span id="lead">Goal</span>: Attract part-time employment through a business or potential freelance clients.</p></li>
+			</ul>
+		</section>
+		</article>
+			</main>
+		<aside><!--Add copy goes here. related but not part of content.--></aside>
+		<footer><!--disclaimer and contact info goes here---></footer>
 	</body>
-</html>
+</html>>
---
+++ underwater.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Deep Dive</title>
+			<link rel="stylesheet" href="css/water.css" type="text/css">
+		</head>
+		<title>Why Cats Scuba Dive</title>
+		<body>
+			<h1>Why Cats Scuba Dive</h1>
+			<p>
+						<span id="ripple">A long</span>
+						<span id="splash">long time</span>
+						<span id="splash2">ago,</span>
+						<span id="splash3">in a</span>
+						<span id="bubblesdown">galaxy</span>
+						<span id="transparentbubbles">far</span>
+						<span id="helmetcat">far</span>
+						<span id="bubble">away&hellip;</span>
+					</p>
+				</body>
+			</html>
