4276b24b4e9831af4f39691f35cd5278c5058964
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-24 09:00:08 -0600

    completed water.css

--- underwater.php
+++ underwater.php
@@ -7,16 +7,15 @@
 		</head>
 		<title>Why Cats Scuba Dive</title>
 		<body>
-			<h1>Why Cats Scuba Dive</h1>
-			<p>
-						<span id="underwater2">A long</span>
-						<span id="splashimg">long time</span>
-						<span id="splash2">ago,</span>
-						<span id="splash3">in a</span>
-						<span id="transparentbubbles">"galaxy</span>
-						<span id="transparentbubbles2">far</span>
-						<span id="helmetcat">far</span>
-						<span id="underwater">away&hellip;</span>
-					</p>
-				</body>
-			</html>
+			<h1>Deep Dive</h1>
+				<p>
+						<span id="clearsplash">Deep</span>
+						<span id="clearsplash2">Dive</span>
+						<span id="helmetcat">coder</span>
+						<span id="splash2">cat</span>
+						<span id="splash3">Carl</span>
+						<span id="transparentbubbles">coded</span>
+						<span id="transparentbubbles2">cookies"&hellip;</span>
+				</p>
+			</body>
+		</html>
