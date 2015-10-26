7f32a6cdb39c448125eecdccd3f8d3236d09ddae
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 05:47:55 -0600

    submission css

--- links/samples.php
+++ links/samples.php
@@ -7,43 +7,19 @@
 </head>
 <body>
 <main>
-<div id="samples">
-	<h1 class="fadeInLeftBig">Click on covers to see Animations</h1>
-</div>
-<article class="sample" role="article">
-	<ul id="galaxy">
-		<li>
-			<a href="../samples/space.php"><img src="/img/space/starry.png" alt="Galaxy"</a>
-			<div class="galaxyinfo">
-				<h3>Space</h3>
-				<p>A starry animation background.</p>
-				<a href="/samples/space.php" title="galaxy">Galaxy CSS</a>
-			</li>
-	</ul>
-	<ul id="water">
-		<li>
-			<img id="water" src="/img/water/scubacat4.jpg" alt="underwater"/>
-			<div class="underwaterinfo"
-			<p>Underwater Dive Surroundings.</p>
-			<a href="/samples/underwater.php" title="Underwater">Underwater CSS</a>
-		</li>
-	</ul>
-</article>
-</main>
-	<footer id="pageFooter">
-		<div class="rightCol">
-			<p>Web Designs by Jennifer Sukay</p>
-			<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</p></div>
-			<p>JennWebDesigns</p>
-		<div class="leftCol"><p>all content @copy; copyright 2015 JennWebDesigns</p>
-			<p><a href="tel://505-489-9573" title="phone"</a></p>
-			<p>Albuquerque, New Mexico</p>
-		</div>
+	<h1>Click on an image to view animation</h1>
+	<div id="star">
+	<a href="../samples/space.php"><img id="star" src="../img/space/starry.png" alt="Galaxy"</a>
+		<footer id="link">
+			<a href="../samples/space.php">Galaxy</a>
+		</footer>
+	</div>
+	<div id="underwater">
+	<a href="/samples/underwater.php"><img id="water" src="../img/water/scubacat4.jpg" alt="underwater"</a>
+	<footer id="link">
+		<a href="../samples/underwater.php">Underwater Dive</a>
 	</footer>
-		<!-- jQuery  -->
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
+	</div>
+</main>
 </body>
 </html>
