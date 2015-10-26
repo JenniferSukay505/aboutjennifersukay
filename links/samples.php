d491ab233e15d02035e65a6f7767c6b1a8c5323e
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 05:20:53 -0600

    submission css

--- links/samples.php
+++ links/samples.php
@@ -1,53 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css" rel="stylesheet">
-		<!-- animate.css -->
-
-		<title>Design Samples</title>
-		<!--css-->
-		<link href="/css/projects.css" rel="stylesheet">
-
-	</head>
-	<body>
-		<main>
-			<div id="samples">
-				<h1 class="fadeInLeftBig">Click on covers to see Animations</h1>
-			</div>
-			<article class="sample" role="article">
-				<ul id="galaxy">
-					<li>
-						<img id="starry" src="/img/space/starry.png" alt="Galaxy"/>
-						<div class="galaxyinfo">
-							<h3>Space</h3>
-							<p>A starry animation background.</p>
-							<a href="/samples/space.php" title="galaxy">Galaxy CSS</a>
-					</li>
-				</ul>
-				<ul id="water">
-					<li>
-						<img id="water" src="/img/water/scubacat4.jpg" alt="underwater"/>
-						<div class="underwaterinfo"
-						<p>Underwater Dive Surroundings.</p>
-						<a href="/samples/underwater.php" title="Underwater">Underwater CSS</a>
-					</li>
-				</ul>
-			</article>
-		</main>
-		<footer id="pageFooter">
-			<div class="rightCol">
-				<p>Web Designs by Jennifer Sukay</p>
-				<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</p></div>
+<head>
+<title>Design Samples</title>
+<!--css-->
+<link href="../css/projects.css" rel="stylesheet">
+</head>
+<body>
+<main>
+<div id="samples">
+	<h1 class="fadeInLeftBig">Click on covers to see Animations</h1>
+</div>
+<article class="sample" role="article">
+	<ul id="galaxy">
+		<li>
+			<a href="../samples/space.php"><img src="/img/space/starry.png" alt="Galaxy"</a>
+			<div class="galaxyinfo">
+				<h3>Space</h3>
+				<p>A starry animation background.</p>
+				<a href="/samples/space.php" title="galaxy">Galaxy CSS</a>
+			</li>
+	</ul>
+	<ul id="water">
+		<li>
+			<img id="water" src="/img/water/scubacat4.jpg" alt="underwater"/>
+			<div class="underwaterinfo"
+			<p>Underwater Dive Surroundings.</p>
+			<a href="/samples/underwater.php" title="Underwater">Underwater CSS</a>
+		</li>
+	</ul>
+</article>
+</main>
+	<footer id="pageFooter">
+		<div class="rightCol">
+			<p>Web Designs by Jennifer Sukay</p>
+			<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</p></div>
 			<p>JennWebDesigns</p>
-			<div class="leftCol"><p>all content @copy; copyright 2015 JennWebDesigns</p>
-				<p><a href="tel://505-489-9573" title="phone"</a></p>
-				<p>Albuquerque, New Mexico</p>
-			</div>
-		</footer>
+		<div class="leftCol"><p>all content @copy; copyright 2015 JennWebDesigns</p>
+			<p><a href="tel://505-489-9573" title="phone"</a></p>
+			<p>Albuquerque, New Mexico</p>
+		</div>
+	</footer>
 		<!-- jQuery  -->
 
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-	</body>
+</body>
 </html>
