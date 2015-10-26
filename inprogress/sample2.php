70a47f494d079ceb0943323ce5074fc3981fe879
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-25 23:57:23 -0600

    wow... still fixing css

---
+++ inprogress/sample2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>Design Samples</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/sample2.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+			<header>
+				<h1 id="lead"> CSS Samples</h1>
+			</header>
+			<main>
+				<article class="sample" role="article">
+					<ul id="galaxy">
+						<li>
+							<img src="../img/space/starry.png" alt="Galaxy"/>
+							<div class="galaxyinfo">
+								<h3>Space</h3>
+								<p>A starry animation background.</p>
+								<a href="../samples/space.php" title="galaxy">Galaxy CSS</a>
+						</li>
+					</ul>
+					<ul id="underwater">
+						<li>
+							<img src="../img/water/scubacat4.jpg" alt="underwater"/>
+							<div class="underwaterwaterinfo"
+							<p>Underwater dive surroundings.</p>
+							<a href="../samples/underwater.php" title="Underwater">Underwater CSS"</a>
+						</li>
+					</ul>
+				</article>
+			</main>
+			<footer id="pageFooter">
+				<div class="rightCol">
+					<p>Web Designs by Jennifer Sukay</p>
+					<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</p></div>
+				<p>JennWebDesigns</p>
+				<div class="leftCol"><p>all content @copy; copyright 2015 JennWebDesigns</p>
+					<p><a href="tel://505-489-9573" title="phone"</a></p>
+					<p>Albuquerque, New Mexico</p>
+				</div>
+			</footer>
+		</body>
+</html>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
