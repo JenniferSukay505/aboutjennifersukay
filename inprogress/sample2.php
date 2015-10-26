98f00c16353fe6228ccd991d5e8400980f9356f6
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 01:39:57 -0600

    working on samples

--- inprogress/sample2.php
+++ inprogress/sample2.php
@@ -49,6 +49,5 @@
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
