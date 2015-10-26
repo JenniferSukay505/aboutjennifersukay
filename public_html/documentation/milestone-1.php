2002df0f57d9da82017c0fdb1379d08ed9c3ece2
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 05:04:40 -0600

    submission css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,65 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="../../css/styles.css" type="text/css">
-<title>About Jennifer Sukay</title>
-</head>
-<body>
-<header id="mainHeader"><!--branding and navigation-->
-<nav role="navigation">
-	<ul>
-		<li><a href="../../links/samples.php" title="Design Samples">Design Samples</a></li>
-		<li><a href="../../links/projects.php" title="Projects">Projects</a></li>
-		<li><a href="../../links/contact.php" title="Contact">Contact</a></li>
-	</ul>
-</nav>
-</header>
-<main id="aboutjennifer" role="main">
-	<header>
-		<h1>About Jennifer Sukay</h1>
-		<figure>
-			<figcaption>Coding Student at <a href="https://bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></figcaption>
-			<img id="cat" src="../../img/water/catscuba2.png" alt="coding">
-		</figure>
-	</header>
-	<section>
-		<h2>Currently exploring web-design and business through study and implementation.</h2>
-	</section>
-	<section id="goal">
-	<article id="purpose">
-			<dl>
-				<dt>Offering</dt>
-				<dd>Light website design services.</p></dd>
-				<dd>Connect and participate in interesting projects</dd>
-				<dt><span id="lead">Audience</span></dt>
-				<dd>Entrepreneurs looking to have their website designed.</p></dd>
-				<dt><span id="lead">Goals</span></dt>
-				<dd>Attract part-time employment through a business or potential freelance clients.</dd>
-			<dd>Garner participation and interest in my own projects</dd>
-			</dl>
-		</section>
-	</main>
-<section id="interests">
-	<h2>Life</h2>
-	<article role="article">
-		<header>
-			<h3><a href="currently.php" title="What I have been up to">What I have been up to</a></h3>
-			<p class="byline">by Jennifer Sukay</p>
-			<p class="pubdate">submitted on
-				<time datetime="2015-10-25">10/25/15</time>
-			</p>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../../css/styles.css" type="text/css">
+		<title>About Jennifer Sukay</title>
+	</head>
+	<body>
+		<header id="mainHeader"><!--branding and navigation-->
+			<nav role="navigation">
+				<ul>
+					<li><a href="/links/samples.php" title="Design Samples">Design Samples</a></li>
+					<li><a href="/links/projects.php" title="Projects">Projects</a></li>
+					<li><a href="/links/contact.php" title="Contact">Contact</a></li>
+				</ul>
+			</nav>
 		</header>
-		<h4 id="lead"><a href="/links/projects.php" title="projects">Pet projects</h4>
-		<p>I have held two project ideas in my heart for years.  Now that I have jumped into coding, I am learning to
-implement them.  Please view my current <a href="/links/projects.php" title="projects">projects</a> and let me know if you want to contribute.
-I am also interested in hearing your own ideas. </p>
-		</article>
-	</section>
-<aside id="adCopy" role="complementary">
-	<h3>Need a website?</h3>
-	<img src="../../img/art2.png" alt="Jenn'sDesigns">
-	<p><a href="/pwp/webdesigns.php" title="Jenn's Web Designs">Gotcha covered!</a></p>
+		<main id="aboutjennifer" role="main">
+			<header>
+				<h1>About Jennifer Sukay</h1>
+				<figure>
+					<figcaption>Coding Student at <a href="https://bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></figcaption>
+					<img id="cat" src="../../img/water/catscuba2.png" alt="coding">
+				</figure>
+			</header>
+			<section>
+				<h2>Currently exploring web-design and business through study and implementation.</h2>
+			</section>
+			<article id="purpose">
+				<section id="goals">
+					<dl>
+						<dt>Offering</dt>
+						<dd>Light website design services.</p></dd>
+						<dd>Connect and participate in interesting projects</dd>
+						<dt><span id="lead">Audience</span></dt>
+						<dd>Entrepreneurs looking to have their website designed.</p></dd>
+						<dt><span id="lead">Goal</span></dt>
+						<dd>Attract part-time employment through a business or potential freelance clients.</dd>
+						<dd>Garner participation and interest in my own projects</dd>
+					</dl>
+				</section>
+		</main>
+		<section id="interests">
+			<h2>Life</h2>
+			<article role="article">
+				<head>
+					<h3>What I have been up to</h3>
+					<p class="byline">by Jennifer Sukay</p>
+					<p class="pubdate">submitted on
+						<time datetime="2015-10-25">10/25/15</time>
+					</p>
+				</head>
+				<h4 id="lead">Designing pet projects</h4>
+				<p>I have had two project ideas in my heart for years.  Now that I have jumped into coding, I am learning to
+					implement them.  Please view my current <a href="/links/projects.php" title="projects">projects</a> and let me know if you want to contribute.
+					I am also interested in hearing your own ideas. </p>
+			</article>
+		</section>
+		<aside id="adCopy" role="complementary">
+			<h3>Need a website?</h3>
+			<img src="../../img/art2.png" alt="Jenn'sDesigns">
+			<p><a href="/pwp/webdesigns.php" title="Jenn's Web Designs" rel="external nofollow">Gotcha covered!</a></p>
 			<p>If you need a website designed, poke your head in here.</p>
 		</aside>
 		<footer id="pageFooter">
@@ -72,4 +72,3 @@ I am also interested in hearing your own ideas. </p>
 		</footer>
 	</body>
 </html>
-
