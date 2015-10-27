20daa5539b30741f686c9a1fb17f53a8b6584d7d
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-27 13:21:44 -0600

    streamlining formatting

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,9 +6,10 @@
 <title>About Jennifer Sukay</title>
 </head>
 <body>
-<header id="mainHeader" role="banner"><!--branding and navigation-->
-	<h1><a id="headerTitle" href="milestone-1.php" title="Jenn's Designs">Jenn's Designs</a></h1>
-	<p>Advertising and Web Design Source</p>
+<header id="mainHeader" role="banner">
+	<h1>Jenn's Designs</h1>
+	<p id="sub">Advertising and Web Design Source</p>
+	<a href="milestone-1.php"<img id="designLink" src="/img/art2.png" alt="artLink"</a>
 	<nav role="navigation">
 		<ul>
 			<li><a href="../../links/samples.php" title="Design Samples">Design Samples</a></li>
@@ -17,20 +18,20 @@
 		</ul>
 	</nav>
 </header>
-<main id="aboutjennifer" role="main">
-<header>
+<main id="aboutJennifer" role="main">
+	<header>
 	<h2>About Jennifer Sukay</h2>
 	<figure>
 		<figcaption>Coding Student at <a href="https://bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></figcaption>
 		<img id="cat" src="../../img/water/catscuba2.png" alt="coding">
 	</figure>
-</header>
+	</header>
 	<h2>Currently exploring web-design and business through study and implementation.</h2>
-<article id="purpose" role="main">
+	<article id="purpose" role="main">
 		<section id="goals">
 			<dl>
 				<dt>Offering</dt>
-				<dd>Light website design services.</p></dd>
+				<dd>Light website design services.</dd>
 				<dd>Connect and participate in interesting projects</dd>
 				<dt><span id="lead">Audience</span></dt>
 				<dd>Entrepreneurs looking to have their website designed.</p></dd>
@@ -39,20 +40,21 @@
 				<dd>Garner participation and interest in my own projects</dd>
 			</dl>
 		</section>
-</main>
+	</main>
 <section id="current">
 	<h3>Current Projects</h3>
 	<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email me!</a></p>
 	<article role="article">
-		<header>
-				<h3>Ongoing</h3>
-				<p class="byline">by Jennifer Sukay</p>
-				<p class="pubdate">submitted on
-					<time datetime="2015-10-25">10/25/15</time>
-				</p>
+	<header>
+		<h3>Ongoing</h3>
+		<p class="byline">by Jennifer Sukay</p>
+		<p class="pubdate">submitted on
+			<time datetime="2015-10-25">10/25/15</time>
+		</p>
 		</header>
 		<p>I have had two project ideas in my heart for years.  Now that I have jumped into coding, I am learning to
-implement them.  Please take a look <a href="../../links/projects.php" title="Ongoing Projects">here</a> and let me know if you want to contribute.  I am also interested in hearing your own ideas. </p>
+implement them.  Please take a look <a href="../../links/projects.php" title="Ongoing Projects">here</a> and let me know
+if you want to contribute.  I am also interested in hearing your own ideas. </p>
 	</article>
 	<aside role="complementary">
 		<h3>Need a website?</h3>
@@ -62,8 +64,8 @@ implement them.  Please take a look <a href="../../links/projects.php" title="On
 		</aside>
 	<aside role="complementary">
 		<h3>Ready to learn?</h3>
-		<img src="../../img/codingbootcamp.png" alt="Jenn'sDesigns">
-		<p><a href="//bootcamp-coders.cnm.edu/" title="Learn to Code!">Learn Coding!</a></p>
+		<p>a hrefe="</p><img src="../../img/codingbootcamp.png" alt="JennsDesigns">
+		<p>http://stemuluscenter.org/products/deep-dive-coding-bootcamp</p>
 		<p>A fast way to learn coding.</p>
 	</aside>
 		<footer id="pageFooter">
