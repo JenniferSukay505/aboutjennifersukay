fb9f0de01cf8f5d10738659e7212a99856c1679c
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 19:05:02 -0600

    added to water.css

--- samples/underwater.php
+++ samples/underwater.php
@@ -2,29 +2,27 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="/css/water.css" type="text/css">
+<link rel="stylesheet" href="../css/water.css" type="text/css">
 <title>Deep Dive</title>
 </head>
 <body>
-<nav role="navigation">
-	<ul>
-		<li><a href="/links/samples.php" title="Design Samples">Design Samples</a></li>
-		<li><a href="/links/projects.php" title="Projects">Projects</a></li>
-		<li><a href="/links/contact.php" title="Contact">Contact</a></li>
+	<ul id="nav">
+		<li><a href="../links/samples.php" title="Design Samples">Design Samples</a></li>
+		<li><a href="../links/projects.php" title="Projects">Projects</a></li>
+		<li><a href="../links/contact.php" title="Contact">Contact</a></li>
 	</ul>
-</nav>
-<h1>Deep Dive</h1>
-		<p><span id="clearsplash">Deep</span>
-			<span id="transparentbubbles">Dive</span>
-			<span id="splash2">coder</span>
-			<span id="helmetcat">Cat</span>
-			<span id="splash3">Carl</span>
-			<span id="transparentbubbles2">coded</span>
-			<span id="waterdrops">so many</span>
-			<span id="clearsplash2">Cookies"&hellip;</span>
-		</p>
-	<footer id="lowerRight">
-		<p>JennWebDesigns</p>
-		<p>all content @copy; copyright 2015 JennWebDesigns</p>
-	</footer>
+	<h1>Deep Dive</h1>
+	<p><span id="clearsplash">Deep</span>
+		<span id="transparentbubbles">Dive</span>
+		<span id="splash2">coder</span>
+		<span id="helmetcat">Cat</span>
+		<span id="splash3">Carl</span>
+		<span id="transparentbubbles2">coded</span>
+		<span id="waterdrops">so many</span>
+		<span id="clearsplash2">Cookies"&hellip;</span>
+	</p>
+<footer id="lowerRight">
+	<p>JennWebDesigns</p>
+	<p>all content @copy; copyright 2015 JennWebDesigns</p>
+</footer>
 </html>
