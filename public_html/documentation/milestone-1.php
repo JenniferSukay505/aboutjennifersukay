5218da14376ad64854b5a3c53ec6b7960f439993
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-27 18:43:44 -0600

    and b etter..

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,7 +9,7 @@
 <header id="mainHeader" role="banner">
 	<h1>Jenn's Designs</h1>
 	<p id="sub">Advertising and Web Design Source</p>
-	<a href="milestone-1.php"<img id="designLink" src="/img/art2.png" alt="artLink"</a>
+	<a href="milestone-1.php"<img id="designLink" src="/img/art2.png" alt="artLink"/>
 	<nav role="navigation">
 		<ul>
 			<li><a href="../../links/samples.php" title="Design Samples">Design Samples</a></li>
@@ -19,65 +19,62 @@
 	</nav>
 </header>
 <main id="about">
-	<header>
-		<article id="about">
-			<section>
-		<h2 id="lead">About Jennifer Sukay</h2>
-
+<header>
+<article id="about">
+	<section>
+	<h2 id="lead">About Jennifer Sukay</h2>
 	<figure>
-		<figcaption>Coding Student at <a href="https://bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></figcaption>
+	<figcaption>Coding Student at <a href="https://bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></figcaption>
 		<img id="cat" src="../../img/water/catscuba2.png" alt="coding">
 	</figure>
 	<blockquote>Currently exploring web-design and business through study and implementation.</blockquote>
-			</section>
-		</article>
-		<article id="purpose" role="main">
-		<section id="goals">
-			<dl>
-				<dt>Offering</dt>
-				<dd>Light website design services.</dd>
-				<dd>Connect and participate in interesting projects</dd>
-				<dt><span id="lead">Audience</span></dt>
-				<dd>Entrepreneurs looking to have their website designed.</p></dd>
-				<dt><span id="lead">Goal</span></dt>
-				<dd>Attract part-time employment through a business or potential freelance clients.</dd>
-				<dd>Garner participation and interest in my own projects</dd>
-			</dl>
-		</section>
-	</article>
+	</section>
+</article>
+<article id="purpose" role="main">
+<section id="goals">
+	<dl>
+		<dt>Offering</dt>
+		<dd>Light website design services.</dd>
+		<dd>Connect and participate in interesting projects</dd>
+		<dt><span id="lead">Audience</span></dt>
+		<dd>Entrepreneurs looking to have their website designed.</p></dd>
+		<dt><span id="lead">Goal</span></dt>
+		<dd>Attract part-time employment through a business or potential freelance clients.</dd>
+		<dd>Garner participation and interest in my own projects</dd>
+	</dl>
+</article>
 </main>
+<section id="current">
+	<h3>Current Projects</h3>
+	<a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email me!</a>
 	<article>
-<div id="current">
-	<h2>Current Projects</h2>
-	<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email me!</a></p>
-	<article role="article">
-	<header>
-		<a id="project" href="/links/projects.php">Ongoing Nonprofit</a>
-		<p class="byline">by Jennifer Sukay</p>
-		<p class="pubdate">submitted on
-			<time datetime="2015-10-25">10/25/15</time>
-		</p>
+		<header>
+			<h3><a id="project" href="/links/projects.php">Ongoing Nonprofit</a></h3>
+			<p class="byline">by Jennifer Sukay</p>
+			<p class="pubdate">submitted on
+				<time datetime="2015-10-25">10/25/15</time>
+			</p>
 		</header>
 		<p>I have had two project ideas in my heart for years.  Now that I have jumped into coding, I am learning to
 implement them.  Please take a look <a href="../../links/projects.php" title="Ongoing Projects">here</a> and let me know
 if you want to contribute.  I am also interested in hearing your own ideas. </p>
 	</article>
-	<aside id="adCopy">
-		<h3>Need a website?</h3>
-			<img src="../../img/art2.png" alt="Jenn'sDesigns">
-			<p><a href="/pwp/webdesigns.php" title="Jenn's Web Designs" >Gotcha covered!</a></p>
-			<p>If you need a website designed, poke your head in here.</p>
-		</aside>
+<aside id="adCopy">
+	<h3>Need a website?</h3>
+	<img src="../../img/art2.png" alt="Jenn'sDesigns">
+	<p><a href="/pwp/webdesigns.php" title="Jenn's Web Designs" >Gotcha covered!</a></p>
+	<p>If you need a website designed, poke your head in here.</p>
+</aside>
 	<aside id="adCopy" role="complementary">
-		<h3>Ready to learn?</h3>
-		<img src="../../img/codingbootcamp.png" alt="JennsDesigns">
-		<p><a href="//stemuluscenter.org/products/deep-dive-coding-bootcamp">Look Here!</a></p>
-		<p>A fast way to learn coding.</p>
-	</aside>
-		<footer id="pageFooter">
-			<div class="rightCol">
-				<p class="topLink"><a href="#mainHeader">return to top</a></p>
-				<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</a></p></div>
+	<h3>Ready to learn?</h3>
+	<img src="../../img/codingbootcamp.png" alt="JennsDesigns">
+	<p><a href="//stemuluscenter.org/products/deep-dive-coding-bootcamp">Look Here!</a><br>
+	<p>A fast way to learn coding.</p>
+</aside>
+	<footer id="pageFooter">
+		<div class="rightCol">
+			<p class="topLink"><a href="#mainHeader">return to top</a></p>
+			<p><a href="mailto:jennifersukay505@gmail.com" title="email Jennifer!">Email</a></p></div>
 			<p>JennWebDesigns</p>
 			<div class="leftCol"><p>all content @copy; copyright 2015 JennWebDesigns</p>
 				<p><a href="tel://505-489-9573" title="phone"</a></p>
